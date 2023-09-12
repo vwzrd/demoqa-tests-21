3a7d2a8e020de9829fb689c1c0c88b48e6193c9e
Maxim_Gavrikov <destrallhumn@gmail.com>
2023-09-12 23:35:14 +0300

    /

--- src/test/java/com/demoqa/pages/components/CalendarComponent.java
+++ src/test/java/com/demoqa/pages/components/CalendarComponent.java
@@ -6,9 +6,8 @@ public class CalendarComponent {
 
     public CalendarComponent setDate(String day, String month, String year) {
         $(".react-datepicker__month-select").selectOption(month);
-        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
         $(".react-datepicker__year-select").selectOption(year);
-
+        $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
 
         return this;
     }
