ed16b64aaf9e00c30ae711d88a581e77a42e80a1
Maxim_Gavrikov <destrallhumn@gmail.com>
2023-09-14 23:56:20 +0300

    .

--- src/test/java/com/demoqa/tests/RemoteTestBase.java
+++ src/test/java/com/demoqa/tests/RemoteTestBase.java
@@ -20,7 +20,7 @@ public class RemoteTestBase {
         Configuration.browserVersion = System.getProperty("browser_version", "100");
         Configuration.pageLoadStrategy = "eager";
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.baseUrl = System.getProperty("base_url");
+        Configuration.baseUrl = System.getProperty("base_url", "https://demoqa.com/");
         Configuration.remote = System.getProperty("remote_driver");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
